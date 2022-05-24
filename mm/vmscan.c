2b5889227f54a1a52e5d8a3b6c47e9199abefd83
Juhyung Park <qkrwngud825@gmail.com>
2022-05-24 22:17:42 +0900

    mm: multi-gen LRU: log when min_ttl is unsatisfied
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    (cherry picked from commit 1064ee9b7b2ceeb690c450a2aa9f98c8245826a7)
    (cherry picked from commit 6b4d19273edd454995a8e6431de9d0e0f2ee5a7e)
    (cherry picked from commit 69e31c4f8ecec6d0a9c69e10253fbfcdbf60db19)
    (cherry picked from commit 28380341cfb8e6b16a56f571f7a718f11cc834b8)
    (cherry picked from commit 4a66234e1712ef9ab521467d0e82763ba85ae90a)
    (cherry picked from commit 4e9ec223877fbd47d092035f0a8b1bb99a53cfe3)
    (cherry picked from commit 5413a7818afdfa0531fdcbf7f72699df1d74db52)
    (cherry picked from commit 1d1da8b8a508eff094bd6db1afa27f878ad765d8)
    (cherry picked from commit eed4d1464796f15910463af786ff312a4be050b2)

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -4626,6 +4626,7 @@ static void lru_gen_age_node(struct pglist_data *pgdat, struct scan_control *sc)
 	 * either too small or below min.
 	 */
 	if (mutex_trylock(&oom_lock)) {
+		pr_err("mglru: min_ttl unsatisfied, calling OOM killer\n");
 		struct oom_control oc = {
 			.gfp_mask = sc->gfp_mask,
 		};
